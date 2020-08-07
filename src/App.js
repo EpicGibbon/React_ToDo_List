12826db2fe89c176082d369ff04fa4b3c288dc96
EpicGibbon <michant1997@gmail.com>
2020-08-06 22:04:35 -0400

    have added functionality to the todolist

--- src/App.js
+++ src/App.js
@@ -24,8 +24,15 @@ class App extends React.Component {
     ]
   }
 
-  markComplete = () => {
-    
+  markComplete = (id) => {
+  this.setState({ todos: this.state.todos.map(todo => {
+    if(todo.id === id) {
+      todo.completed = !todo.completed
+    }
+    return todo;
+  }) });
+  
+  
   }
 
   render() {
--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -2,36 +2,31 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 
 export class TodoItem extends Component {
-getStyle = () => {
-    return {
-        //ternary operator (condition) (? = IF TRUE aka make it line-through) (: represents ELSE)  
-        textDecoration: this.props.todo.completed ? 
-        'line-through' : 'none',
-        background: '#f4f4f4',
-        padding: '10px',
-        borderBottom: '1px #ccc dotted',
+    getStyle = () => {
+        return {
+            //ternary operator (condition) (? = IF TRUE aka make it line-through) (: represents ELSE)  
+            textDecoration: this.props.todo.completed ?
+                'line-through' : 'none',
+            background: '#f4f4f4',
+            padding: '10px',
+            borderBottom: '1px #ccc dotted',
+        }
     }
-}
-
-
-markComplete = (e) => {
-
-}
-
-
     render() {
+        const { id, title } = this.props.todo;
+
         return (
             <div style={this.getStyle()}>
                 <p>
-                    <input type="checkbox" onChange={this.props.markComplete}/> {' '}
-                    { this.props.todo.title }</p>
+                    <input type="checkbox" onChange={this.props.markComplete.bind(this, id)} /> {' '}
+                    {title}</p>
             </div>
         )
     }
 }
 
 TodoItem.propTypes = {
-    todos: PropTypes.array.isRequired
+    todo: PropTypes.object.isRequired
 }
 
-export default TodoItem
+export default TodoItem;
