6060f7257fff6c5c039f4336554a7071ada43abc
EpicGibbon <michant1997@gmail.com>
2020-08-09 16:10:20 -0400

    added Router and page routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Todos from './Components/Todos';
 import Header from './Components/layout/Header';
 import AddTodo from './Components/AddTodo';
-import {v4 as uuid} from "uuid"; 
+import About from './Components/pages/About';
+import { v4 as uuid } from "uuid";
 
 class App extends React.Component {
 
@@ -57,16 +59,23 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="App">
-        <div className="container">
-          <Header />
-          <AddTodo addTodo={this.addTodo} />
-          <Todos
-            todos={this.state.todos}
-            markComplete={this.markComplete}
-            delTodo={this.delTodo} />
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route exact path="/" render={props => (
+              <React.Fragment>
+                <AddTodo addTodo={this.addTodo} />
+                <Todos
+                  todos={this.state.todos}
+                  markComplete={this.markComplete}
+                  delTodo={this.delTodo} />
+              </React.Fragment>
+            )} />
+            <Route path="/about" component={About} />
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
